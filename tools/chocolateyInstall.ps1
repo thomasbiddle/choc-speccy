eb2090f6548c95f8725cbaf756eaadaa5288e644
TJ Biddle <tjbiddle@inflection.com>
2013-05-19 00:48:43 -0700

    Adding localisation

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,8 @@
 #Set the standard 4 parameters
 $packageName = 'speccy'
 $fileType = 'exe'
-$silentArgs = "/S"
+$LCID = (Get-Culture).LCID
+$silentArgs = '/S /L=$LCID'
 $url = 'https://www.piriform.com/speccy/download/slim/downloadfile'
 
 Install-ChocolateyPackage $packageName $fileType $silentArgs $url
