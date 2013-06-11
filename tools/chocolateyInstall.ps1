85b01b8e126427069da9ac25e3a8cdec25a96379
TomOne <user.email@fake.com>
2013-06-11 23:42:00 +0200

    alternative static download link, now uses wget, fix double quotes around  variable

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,29 @@
-#Set the standard 4 parameters
+﻿#Set the standard 4 parameters
 $packageName = 'speccy'
 $fileType = 'exe'
 $LCID = (Get-Culture).LCID
-$silentArgs = '/S /L=$LCID'
-$url = 'https://www.piriform.com/speccy/download/slim/downloadfile'
+$silentArgs = "/S /L=$LCID"
+# Check this page for new versions: http://biblprog.org.ua/en/speccy/
+# Please don’t use the normal version of speccy as it includes and would install silently toolbars or probably unwanted third-party adware. Chocolatey users don’t like that.
+# A new slim version of Speccy gets released about a week after the normal version. Please be patient, wait for the new slim version and link it here.
+$url = 'http://files2.biblprog.org.ua/soft/S/Speccy/spsetup121_slim.exe'
+$filePath = "$env:TEMP\chocolatey\$packageName"
+$fileFullPath = "$filePath\$packageName`Install.exe"
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+
+# Use wget to download this and use --referer=http://biblprog.org.ua/en/speccy/
+
+try {
+	if (-not (Test-Path $filePath)) {
+		New-Item -ItemType directory -Path $filePath
+	}
+
+	Start-Process "wget" -ArgumentList "--referer=http://biblprog.org.ua/en/speccy/ $url -O `"$fileFullPath`"" -Wait
+
+	Install-ChocolateyInstallPackage $packageName $fileType $silentArgs $fileFullPath
+	
+    Write-ChocolateySuccess "$packageName"
+} catch {
+    Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+    throw
+}
