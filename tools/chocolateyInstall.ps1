34eab12ad71c52af885b0c85e41949f58631c4b9
TomOne <user.email@fake.com>
2013-07-07 23:53:57 +0200

    switched to static download link to normal speccy version, added workaround to avoid the installation of bundled adware, icon with better resolution, corrections in nuspec, fixed single quote issue

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,21 @@
-#Set the standard 4 parameters
+﻿# Set the standard 4 parameters
 $packageName = 'speccy'
 $fileType = 'exe'
 $LCID = (Get-Culture).LCID
-$silentArgs = '/S /L=$LCID'
-$url = 'https://www.piriform.com/speccy/download/slim/downloadfile'
+$silentArgs = "/S /L=$LCID"
+# Please test every new version of Speccy for possible adware/spyware/crapware which installs silently together with Speccy.
+# Only push the new package to the gallery if you are 100 % sure that this package prevents the install of the bundled adware.
+$url = 'http://download.piriform.com/spsetup122.exe'
+$regAdd = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)\regAdd.ps1"
 
-Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+try {
+    # This adds a registry key which prevents Google Chrome from getting installed together with Speccy
+    Start-ChocolateyProcessAsAdmin "& `'$regAdd`'"
+
+    Install-ChocolateyPackage $packageName $fileType $silentArgs $url
+
+    Write-ChocolateySuccess "$packageName"
+} catch {
+    Write-ChocolateyFailure "$packageName" "$($_.Exception.Message)"
+    throw
+}
